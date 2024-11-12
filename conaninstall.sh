c56b39c65bfc6770c720ca79286dfc5cb4bea65a
zerofivefiveseven <rv4p@ya.ru>
2024-11-12 09:18:22 -0400

    ADD: gitignore EDIT: conan scripts

--- conaninstall.sh
+++ conaninstall.sh
@@ -1,2 +1,1 @@
 conan install . --output-folder=cmake-build-debug --build=missing --settings=build_type=Debug
-#cmake -G "Ninja" -S . -B cmake-build-debug -DCMAKE_TOOLCHAIN_FILE=/home/thiago/src/my-project/build/build/Debug/generators/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Debug
