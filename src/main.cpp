b8fd1aaa83a9b43c944adbc33536da2bfc9263ce
zerofivefiveseven <rv4p@ya.ru>
2024-11-15 17:26:05 +0300

    ADD: gen files by flatc with .fbs schema & support new headers in cmake project

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include "flatbuffers/flatbuffers.h"
-#include "cxxopts.hpp"
+#include <cxxopts.hpp>
 #include "monster_generated.h"
 int main(int argc, char** argv)
 {
